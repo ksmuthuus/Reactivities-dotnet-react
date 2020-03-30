8f5f4e22b187cb4532c89cac38e5c1eb6d5ee06c
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2020-03-30 10:04:01 +0530

    Added Activities route for list and details

---
+++ Api/Controllers/ActivitiesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Application;
+using Application.Activities;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ActivitiesController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> List()
+        {
+            return await _mediator.Send(new List.Query());
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            return await _mediator.Send(new Details.Query { Id = id });
+        }
+    }
+}
